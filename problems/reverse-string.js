6a5a8ed7e83093a5089119047a4a2473a8b61a50
Ruben Polanco <andr3z.93@gmail.com>
2023-09-13 21:32:15 -0700

    answer to reverse

--- problems/reverse-string.js
+++ problems/reverse-string.js
@@ -1,3 +1,6 @@
+
+
+
 module.exports = function reverseString(string) {
-  // Your code here
+  return string.split("").reverse().join("")
 };
--- test/reverse-string-spec.js
+++ test/reverse-string-spec.js
@@ -1,1 +1,9 @@
-// Your code here
+const { expect } = require("../node_modules/chai")
+const reverseString = require("../problems/reverse-string.js")
+
+
+describe ("testing reverseString",  () => {
+    it('should reverse the string', () => {
+        expect(reverseString("fun")).to.equal('nuf');
+    })
+})
